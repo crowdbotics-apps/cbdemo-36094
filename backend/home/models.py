3459fdbfc053ba7bb180cb0c10953e47fee8533c
crowdbotics-apps <team@crowdbotics.com>
2023-03-22 11:09:30 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Client(models.Model):
+    'Generated Model'
+    user = models.ForeignKey("users.User",on_delete=models.CASCADE,related_name="client_user",)
+    name = models.CharField(max_length=256,)
+    age = models.PositiveIntegerField()
